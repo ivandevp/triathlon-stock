65acf077bcfb16da3bc223767520e9cff2848425
Ivan Medina <medinadiazivan@gmail.com>
2015-10-15 22:12:26 -0500

    Platform and extra resources

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,25 +1,17 @@
-var app = angular.module('triathlonApp', []);
+var app = angular.module('triathlonApp', [
+	'ngRoute',
+	'triathlon.controllers'
+]);
 
-app.controller('mainController', function($scope, $http) {
-	$scope.message = 'Holaaa!';
-	$scope.user.pin = '';
-	$scope.user.login = function(user) {
-		console.log("User : " + user);
-		var response = $http.get("http://192.168.1.40/tapi/api/employee/" + user);
-		response.success(function(data, status, headers, config) {
-			if (data == null) {
-				$scope.message = "Usuario no existe";
-                navigator.notification.alert("PIN INCORRECTO");
-                console.log("Usuario no existe!!");
-            } else {
-                navigator.notification.alert("BIENVENIDO =)");
-                console.log("Usuario existe!!");
-                $scope.message = "Usuario correcto: " + data.FirstName + " " + data.LastName;
-            }
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/login.html'
+		})
+		.when('/home', {
+			templateUrl: 'views/home.html'
+		})
+		.otherwise({
+			redirectTo: '/'
 		});
-		response.error(function(data, status, headers, config) {
-			console.log("Un error ha ocurrido!!");
-		});
-	}
-
-});
+}]);
